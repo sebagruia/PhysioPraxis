fd87361c2667bb02f7b015034249d2a1ee5132f3
sebagruia <sebastiangruia@gmail.com>
2020-08-07 09:55:44 +0200

    Updates

---
+++ precache-manifest.b50b99fd947d220c531e5f636710530e.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d6d9d77ad355b93a8722eff6160fc1f9",
+    "url": "/PhysioPraxis/index.html"
+  },
+  {
+    "revision": "9f699453d260c6240d2b",
+    "url": "/PhysioPraxis/static/css/main.40f1a262.chunk.css"
+  },
+  {
+    "revision": "28daa3d3d13eeb6398f5",
+    "url": "/PhysioPraxis/static/js/2.74d22b6e.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/PhysioPraxis/static/js/2.74d22b6e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9f699453d260c6240d2b",
+    "url": "/PhysioPraxis/static/js/main.0a9c77c2.chunk.js"
+  },
+  {
+    "revision": "7578d63ac553603c662c",
+    "url": "/PhysioPraxis/static/js/runtime-main.3f77e469.js"
+  },
+  {
+    "revision": "52ed50ad821fa4504af953618497edc7",
+    "url": "/PhysioPraxis/static/media/PhysiopraxisLogoOuterGlow.52ed50ad.png"
+  },
+  {
+    "revision": "6728a7ac18e3ac0a4c39d73e2552398b",
+    "url": "/PhysioPraxis/static/media/PhysiopraxisLogoShadow.6728a7ac.png"
+  },
+  {
+    "revision": "2e64a1f30608c68de2e1350207814f5a",
+    "url": "/PhysioPraxis/static/media/doctor1.2e64a1f3.png"
+  },
+  {
+    "revision": "4629ae843fdd523ea768ce2fc264e415",
+    "url": "/PhysioPraxis/static/media/hero_img_reduced.4629ae84.jpg"
+  }
+]);
