8dc39a5df4a4ebab862f55b3c26e72eda0f78be9
sebagruia <sebastiangruia@gmail.com>
2020-09-17 20:06:42 +0200

    Fixed some issues with image dimensions in Safari browser

--- src/DATA.js
+++ src/DATA.js
@@ -255,9 +255,9 @@ export const testimonialsContent = {
 // BootstrapCarousel Images
 
 export const carouselImages = [
-  'https://firebasestorage.googleapis.com/v0/b/physiopraxis-51c08.appspot.com/o/carouselImages%2Fcarousel4.webp?alt=media&token=4e850fe0-f0e5-4e73-baa1-fd15313925e6',
+  'https://firebasestorage.googleapis.com/v0/b/physiopraxis-51c08.appspot.com/o/carouselImages%2Fcarousel4.jpg?alt=media&token=a685c404-5210-432e-8402-c964435e22f2',
   'https://firebasestorage.googleapis.com/v0/b/physiopraxis-51c08.appspot.com/o/carouselImages%2Fcarousel2.jpg?alt=media&token=48f61a1f-39e1-4b82-97e3-221d555ab7a4',
-  'https://firebasestorage.googleapis.com/v0/b/physiopraxis-51c08.appspot.com/o/carouselImages%2Fcarousel3.webp?alt=media&token=996d4194-38bd-483a-899c-92c3719dc626',
+  'https://firebasestorage.googleapis.com/v0/b/physiopraxis-51c08.appspot.com/o/carouselImages%2Fcarousel3.jpg?alt=media&token=0680467a-800a-4568-9fd0-0fa6d8b3993e',
   'https://firebasestorage.googleapis.com/v0/b/physiopraxis-51c08.appspot.com/o/carouselImages%2Fcarousel5.jpg?alt=media&token=29be9c49-e611-44b5-b07b-a4d3ed8246ee',
   'https://firebasestorage.googleapis.com/v0/b/physiopraxis-51c08.appspot.com/o/carouselImages%2Fcarousel61.jpg?alt=media&token=c8a2ccee-925f-4322-b1b6-f094efe8366d',
   'https://firebasestorage.googleapis.com/v0/b/physiopraxis-51c08.appspot.com/o/carouselImages%2Fcarousel7.jpg?alt=media&token=c70a3bb4-a541-4408-8ca1-85426efd2bb3',
