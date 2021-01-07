c824161e06af767364d4e8eafeede57514c9f436
sebagruia <blackthorus@gmail.com>
2021-01-07 13:01:49 +0100

    Fixed LoadSpinner component position

--- src/components/LoadingSpinner/loadingSpinner.js
+++ src/components/LoadingSpinner/loadingSpinner.js
@@ -3,12 +3,14 @@ import "./loadingSpinner.css";
 
 const loadingSpinner = () => {
   return (
-    <div className="loadingSpinner">
-      <div className="loadingSpinner-1">
-        <div className="loadingSpinner-2">
-          <div className="loadingSpinner-3">
-            <div className="loadingSpinner-4">
-              <div className="loadingSpinner-5"></div>
+    <div className="loadingSpinner_container">
+      <div className="loadingSpinner">
+        <div className="loadingSpinner-1">
+          <div className="loadingSpinner-2">
+            <div className="loadingSpinner-3">
+              <div className="loadingSpinner-4">
+                <div className="loadingSpinner-5"></div>
+              </div>
             </div>
           </div>
         </div>
