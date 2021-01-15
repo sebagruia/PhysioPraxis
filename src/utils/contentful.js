ddc681f330026c1fc7f3f318b83eb5649922f6e6
sebagruia <blackthorus@gmail.com>
2021-01-15 15:26:41 +0100

    Added contentful package to project

--- src/utils/contentful.js
+++ src/utils/contentful.js
@@ -1,7 +1,8 @@
 const contentful = require("contentful");
-const client = contentful.createClient({
+export const client = contentful.createClient({
   // This is the space ID. A space is like a project folder in Contentful terms
-  space: "developer_bookshelf",
+  environment: 'master',
+  space:process.env. REACT_APP_CONTENTFUL_SPACE,
   // This is the access token for this space. Normally you get both ID and the token in the Contentful web app
-  accessToken: "0b7f6x59a0"
+  accessToken: process.env.REACT_APP_CONTENTFUL_DELIVERY_API_ACCES_TOKEN;
 });
