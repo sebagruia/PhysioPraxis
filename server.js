1e6f86f95939995cd65e8c09cd622baeb0990f85
sebagruia <blackthorus@gmail.com>
2021-01-19 11:28:46 +0100

    Fixed server.js

--- server.js
+++ server.js
@@ -39,7 +39,7 @@ app.get("/contentful/testimonialsHome", async (req, res) => {
   res.send(testimonialsHome);
 });
 
-app.get("*", (req, res) => {
+app.get("/*", (req, res) => {
   res.sendFile(path.join(__dirname, "build", "index.html"));
 });
 
